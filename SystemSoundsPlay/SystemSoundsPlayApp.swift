1c872e6d34396b5bc93219de77d920433eee7556
JoSmith40 <jo.smith40@yahoo.com>
2025-09-20 10:03:38 +0200

    Initial Commit

---
+++ SystemSoundsPlay/SystemSoundsPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SystemSoundsPlayApp.swift
+//  SystemSoundsPlay
+//
+//  Created by Juergen on 20.09.25.
+//
+
+import SwiftUI
+
+@main
+struct SystemSoundsPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
